431ad5c5d48357ef405b420c60a6db0ba47bcb5a
Gabriel <caampos22@gmail.com>
2023-12-14 12:21:23 -0300

    Fix 🔧: Fix env variables

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+  },
   images: {
     remotePatterns: [
       {
--- src/lib/firebase-config.ts
+++ src/lib/firebase-config.ts
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app'
 
 export const firebaseApp = initializeApp({
-  apiKey: 'AIzaSyD5Cybg6fSXjbiAhrJ_frn3KC4S7QnDj3k',
-  authDomain: 'study-9a623.firebaseapp.com',
-  projectId: 'study-9a623',
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
 })
