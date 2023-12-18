ff38e5e23cb836f9139cdc6b73ea697c79a4a74c
Gabriel <caampos22@gmail.com>
2023-12-17 23:07:30 -0300

    Feat ✨: Add skeleton to event overview page

--- src/components/events/event-overview.tsx
+++ src/components/events/event-overview.tsx
@@ -18,10 +18,58 @@ import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { Button } from '@/components/ui/button'
 import { toast } from '../ui/use-toast'
+import { Skeleton } from '../ui/skeleton'
 
 const formSchema = z.object({
   content: z.string(),
 })
+
+function LoadingSkeleton() {
+  return (
+    <main className="flex flex-col flex-1 w-full max-w-4xl p-4 mx-auto">
+      <ChevronLeft className="w-8 h-8" />
+      <div className="flex flex-col items-center w-full max-w-md mx-auto mt-2">
+        <Skeleton className="w-[450px] h-[450px] shadow-md rounded-b-none" />
+        <div className="flex flex-col w-full max-w-md px-4 py-2 bg-white rounded-b-md">
+          <div className="flex flex-row items-center justify-between">
+            <div className="flex flex-row items-center">
+              <Skeleton className="w-8 h-8 rounded-full" />
+              <Skeleton className="w-12 h-3 my-1 ml-2" />
+            </div>
+            <Skeleton className="w-8 h-2" />
+          </div>
+          <Separator className="my-1" />
+          <div className="flex flex-col">
+            <Skeleton className="w-32 h-3 my-1" />
+            <Skeleton className="w-24 h-2" />
+            <Skeleton className="w-24 h-2" />
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-row items-center w-full max-w-md gap-2 p-2 mx-auto mt-2 bg-white rounded-md shadow-md">
+        <Skeleton className="w-8 h-8 rounded-full" />
+        <div className="flex justify-between flex-1 gap-2">
+          <div className="flex flex-col flex-1 gap-1">
+            <Skeleton className="w-16 h-4 mb-2" />
+            <Skeleton className="w-1/2 h-3" />
+            <Skeleton className="w-1/3 h-3" />
+            <Skeleton className="w-8 h-2 mt-3" />
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-row items-center w-full max-w-md gap-2 p-2 mx-auto mt-2 bg-white rounded-md shadow-md">
+        <Skeleton className="w-8 h-8 rounded-full" />
+        <div className="flex justify-between flex-1 gap-2">
+          <div className="flex flex-1">
+            <Input placeholder="adicione um comentario" />
+          </div>
+
+          <Button type="submit">Enviar</Button>
+        </div>
+      </div>
+    </main>
+  )
+}
 
 export function EventOverview({ id, event }: { id: string; event: string }) {
   const { challenges, addComment } = useChallenge()
@@ -36,7 +84,9 @@ export function EventOverview({ id, event }: { id: string; event: string }) {
 
   const challenge = challenges.find((challenge) => challenge?.id === id)
 
-  if (!challenge || !user) return
+  if (!challenge || !user) {
+    return <LoadingSkeleton />
+  }
 
   const currentEvent = challenge.events.find(
     (evt: Record<string, unknown>) => evt.id === event,
