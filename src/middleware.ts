85939e4effdf00788cc72e7b54e6a061f95e1348
Gabriel <caampos22@gmail.com>
2024-01-17 19:01:17 -0300

    Fix 🔧: Fix public route

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import { authMiddleware } from '@clerk/nextjs'
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-	publicRoutes: ['/', '/login', '/register'],
+	publicRoutes: ['/', '/login', '/register', '/api/db/create-user'],
 })
 
 export const config = {
