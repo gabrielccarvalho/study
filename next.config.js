451f54269871b1fa3be08c77d7be4fd576c3495a
Gabriel <caampos22@gmail.com>
2024-01-17 21:16:54 -0300

    Fix 🔧: Fix webhook env variable

--- next.config.js
+++ next.config.js
@@ -16,6 +16,7 @@ const nextConfig = {
 		NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
 			process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
 		STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+		WEBHOOK_SECRET: process.env.WEBHOOK_SECRET,
 	},
 	images: {
 		remotePatterns: [
