6a56cd61da8ce6f32242a70e12c36e2445524938
Gabriel <caampos22@gmail.com>
2024-01-23 13:14:03 -0300

    Fix 🔧: Comment pricing page while we don't have any

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Header } from '@/containers/landing-page/header'
 import { HeroSection } from '@/containers/landing-page/hero'
 import { LandingFooter } from '@/containers/landing-page/landing-footer'
-import { Pricing } from '@/containers/landing-page/pricing'
+// import { Pricing } from '@/containers/landing-page/pricing'
 import { StudyTrack } from '@/containers/landing-page/track'
 
 export default function Home() {
@@ -10,7 +10,7 @@ export default function Home() {
 			<Header />
 			<HeroSection />
 			<StudyTrack />
-			<Pricing />
+			{/* <Pricing /> */}
 			<LandingFooter />
 		</>
 	)
